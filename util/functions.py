c57182475adeac398baa0e9930a949d1184fe4df
Taymaz <TaymazKH.U@gmail.com>
2023-02-16 14:57:28 +0330

    added s_box and m_matrix

--- util/functions.py
+++ util/functions.py
@@ -1,7 +1,7 @@
 from baseconv import base2
 
 
-def _extend_number(str_num: str, digit_count: int):
+def extend_number(str_num: str, digit_count: int):
     return '0' * (digit_count - len(str_num)) + str_num
 
 
@@ -9,11 +9,11 @@ def pixel_to_string(pixel, mode):
     if mode == '1':
         return str(pixel)
     elif mode == 'L':
-        return _extend_number(base2.encode(pixel), 8)
+        return extend_number(base2.encode(pixel), 8)
     elif mode == 'RGB' or mode == 'RGBA':
         output = ''
         for num in pixel:
-            output += _extend_number(base2.encode(num), 8)
+            output += extend_number(base2.encode(num), 8)
         return output
     else:
         raise ValueError(f'unsupported image mode: {mode}')
