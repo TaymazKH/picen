28652c1d621bf0d9910ae16979d8ae08c47e8de0
Taymaz <TaymazKH.U@gmail.com>
2023-02-19 18:55:02 +0330

    implemented ImageFileWriter

--- writer/image_file_writer.py
+++ writer/image_file_writer.py
@@ -2,14 +2,16 @@ from writer.writer import Writer
 
 
 class ImageFileWriter(Writer):
-    def __init__(self):
-        pass
+    def __init__(self, path):
+        self.path = path
 
     def write_next_block(self, block: str):
-        pass
+        with open(self.path, 'a') as file:
+            file.write(block + '\n')
 
     def write_init(self, init):
-        pass
+        with open(self.path, 'w') as file:
+            file.write(init[0] + init[1] + '\n')
 
     def write_end(self, end):
         pass
